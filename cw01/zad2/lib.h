390d146ac4928a571954b5a028603df988440cdb
Malenczuk <malen11pl@gmail.com>
2018-03-09 10:52:52 +0100

    .

---
+++ cw01/zad2/lib.h
@@ -0,0 +1,26 @@
+#ifndef zad1_LIB_H
+#define zad1_LIB_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <zconf.h>
+#include <stdbool.h>
+
+typedef struct {
+    char** array;
+    int arraySize;
+    int blockSize;
+    bool isStatic;
+} Array;
+
+extern char staticArray[1000][1000];
+
+Array *createArray(int arraySize, int blockSize, bool isStatic);
+void deleteArray(Array* blockArray);
+void addBlockAtIndex(Array* blockArray, char* block, int index);
+void deleteBlockAtIndex(Array* blockArray, int index);
+int blockToInt(char* block);
+char *findBlock(Array* blockArray, int searchValue);
+
+#endif //zad1_LIB_H
