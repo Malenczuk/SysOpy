ba6fa4b2b52a655654bf984e03927b419047fc0e
Malenczuk <malen11pl@gmail.com>
2018-03-13 20:10:59 +0100

    Gupie SysOpy

--- cw01/zad3a/lib.c
+++ cw01/zad3a/lib.c
@@ -1,6 +1,6 @@
 #include "lib.h"
 
-char staticArray[1000000][1000];
+char staticArray[STATIC_ARRAY_SIZE][STATIC_BLOCK_SIZE];
 
 void mkrndstr(char *block, size_t length) {
     static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?! ";
@@ -20,16 +20,15 @@ void mkrndstr(char *block, size_t length) {
 }
 
 Array *createArray(int arraySize, int blockSize, bool isStatic){
-    if(arraySize <= 0) {
-        printf("Array Size must be positive");
+    if(isStatic && arraySize > STATIC_ARRAY_SIZE) {
+        printf("Maximum Static Array size is %d", STATIC_ARRAY_SIZE);
         return NULL;
     }
-    if(blockSize <= 0) {
-        printf("Block Size must be positive");
+    if(isStatic && blockSize >= STATIC_BLOCK_SIZE) {
+        printf("Maximum Static Block size is %d", STATIC_BLOCK_SIZE - 1);
         return NULL;
     }
     Array* blockArray = calloc(1, sizeof(Array));
-
     if(isStatic)
         blockArray->array = (char **) staticArray;
     else
@@ -85,21 +84,16 @@ int blockToInt(char* block) {
     return result;
 }
 
-char *findBlock(Array* blockArray, int index) {
+char *findBlock(Array* blockArray, int searchValue) {
     char* closestBlock = NULL;
-    if(index >= blockArray->arraySize || index < 0)
-        printf("Index out of bounds");
-    else {
-        int searchValue = blockToInt(blockArray->array[index]);
-        int minDifference = INT_MAX;
-        for(int i = 0; i < blockArray->arraySize; i++){
-            char* block = blockArray->array[i];
-            if(block != NULL){
-                int difference = abs(blockToInt(block) - searchValue);
-                if(minDifference >= difference){
-                    minDifference = difference;
-                    closestBlock = block;
-                }
+    int minDifference = INT_MAX;
+    for(int i = 0; i < blockArray->arraySize; i++){
+        char* block = blockArray->array[i];
+        if(block != NULL){
+            int difference = abs(blockToInt(block) - searchValue);
+            if(minDifference >= difference && difference != 0){
+                minDifference = difference;
+                closestBlock = block;
             }
         }
     }
