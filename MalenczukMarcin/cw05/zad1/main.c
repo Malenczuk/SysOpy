e3791f18e9d17bc03b5fec73fe44156d13433911
Malenczuk <malen11pl@gmail.com>
2018-04-13 10:07:39 +0200

    Dodalem zamknięcie ostatniego pipa bo sie nie konczl proces

--- MalenczukMarcin/cw05/zad1/main.c
+++ MalenczukMarcin/cw05/zad1/main.c
@@ -12,12 +12,12 @@
 #define ARGS_MAX 64
 #define CMDS_MAX 64
 #define LINE_MAX 256
-
+int pipes[2][2];
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) FAILURE_EXIT(1, "To little Arguments.\n");
-    FILE* file = fopen(argv[1], "r");
-    if(!file) FAILURE_EXIT(1, "Error while opening file %s\n", argv[1]);
+    if (argc < 2) FAILURE_EXIT(1, "To little Arguments.\n");
+    FILE *file = fopen(argv[1], "r");
+    if (!file) FAILURE_EXIT(1, "Error while opening file %s\n", argv[1]);
 
     char r0[LINE_MAX], r1[LINE_MAX];
     char *args[ARGS_MAX], *cmds[CMDS_MAX];
@@ -27,19 +27,15 @@ int main(int argc, char *argv[]) {
         lineNum++;
         strcpy(r1, r0);
         cmdNum = 0;
-        while ((cmds[cmdNum] = strtok(cmdNum == 0 ? r1 : NULL, "|")) != NULL) {
+        while ((cmds[cmdNum] = strtok(cmdNum == 0 ? r1 : NULL, "|\n")) != NULL)
             if (++cmdNum >= CMDS_MAX) FAILURE_EXIT(1, "Line %d exceeds maximum number (%d) of pipes", lineNum, CMDS_MAX)
-        }
         if (!cmdNum) continue;
 
-        int pipes[2][2];
-
-        for (int k = 0; k < cmdNum; k++) {
+        int k;
+        for (k = 0; k < cmdNum; k++) {
             argNum = 0;
-            while ((args[argNum] = strtok(argNum == 0 ? cmds[k] : NULL, " \n")) != NULL) {
-                if (++argNum >= ARGS_MAX) FAILURE_EXIT(1, "Command %d exceeds maximum number (%d) of arguments", k,
-                                                                   ARGS_MAX)
-            }
+            while ((args[argNum] = strtok(argNum == 0 ? cmds[k] : NULL, " \t\n")) != NULL)
+                if (++argNum >= ARGS_MAX) FAILURE_EXIT(1, "Command %d exceeds maximum number (%d) of args", k, ARGS_MAX)
             if (!argNum) continue;
 
             if (k > 1) {
@@ -50,12 +46,12 @@ int main(int argc, char *argv[]) {
                 printf("Couldn't pipe at number %d!\n", k);
 
             pid_t cp = fork();
-            if (cp == -1) {
-                FAILURE_EXIT(1, "Couldnt fork child process %d!\n", k);
-            } else if (cp == 0) {
+            if (cp == -1) FAILURE_EXIT(1, "Couldnt fork child process %d!\n", k);
+            if (cp == 0) {
                 if (k < cmdNum - 1) {
                     close(pipes[k % 2][0]);
                     if (dup2(pipes[k % 2][1], 1) < 0) FAILURE_EXIT(3, "Couldnt set writing at number %d!\n", k);
+                    wait(NULL);
                 }
                 if (k != 0) {
                     close(pipes[(k + 1) % 2][1]);
@@ -65,16 +61,15 @@ int main(int argc, char *argv[]) {
                 FAILURE_EXIT(1, "ERROR EXECUTING CHILD PROCESS %d!\n", k);
             }
         }
-        while (1) {
-            wait(NULL);
+        close(pipes[k % 2][0]);
+        close(pipes[k % 2][1]);
+        while (wait(NULL)) {
             if (errno == ECHILD) {
                 printf("\n");
                 break;
             }
         }
     }
-
-
     fclose(file);
     return 0;
 }
