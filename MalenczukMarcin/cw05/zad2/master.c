45d84de103d60684385ba2c21e1c2a7a82c3f21e
Malenczuk <malen11pl@gmail.com>
2018-04-13 17:32:25 +0200

    jakis syf

--- MalenczukMarcin/cw05/zad2/master.c
+++ MalenczukMarcin/cw05/zad2/master.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#define FAILURE_EXIT(code, format, ...) { fprintf(stderr, format, ##__VA_ARGS__); exit(code);}
+#define BUFF_SIZE 256
+
+int allReady = 0;
+
+void rtHandler(int signo, siginfo_t *info, void *context) {
+    if (signo == SIGRTMIN) allReady = 0;
+}
 
 int main(int argc, char *argv[]) {
+    struct sigaction act;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = rtHandler;
 
+    if (argc < 2) {
+        kill(getppid(), SIGRTMIN + 2);
+        FAILURE_EXIT(1, "Master: To little Arguments.\n");
+    }
+    if (sigaction(SIGRTMIN, &act, NULL) == -1) {
+        kill(getppid(), SIGRTMIN + 2);
+        FAILURE_EXIT(1, "Master: Can't catch SIGRTMIN\n");
+    }
+
+    if (mkfifo(argv[1], S_IRUSR | S_IWUSR) == -1) {
+        kill(getppid(), SIGRTMIN + 2);
+        FAILURE_EXIT(1, "Master: Error creating FIFO\n");
+    }
+    FILE *fifo = fopen(argv[1], "r");
+    if (!fifo) {
+        kill(getppid(), SIGRTMIN + 2);
+        FAILURE_EXIT(1, "Master: Error opening FIFO\n")
+    }
+    kill(getppid(), SIGRTMIN + 1);
+
+    while (!allReady) pause();
+
+    char buffer[BUFF_SIZE];
+    while (fgets(buffer, BUFF_SIZE, fifo) != NULL) {
+        write(1, buffer, strlen(buffer));
+    }
+    printf("Master: Ended reading FIFO\n");
+    fclose(fifo);
+    if (remove(argv[1])) {
+        kill(getppid(), SIGRTMIN + 2);
+        FAILURE_EXIT(1, "Master: Error deleting FIFO\n");
+    }
     return 0;
 }
--- MalenczukMarcin/cw05/zad2/slave.c
+++ MalenczukMarcin/cw05/zad2/slave.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <signal.h>
+
+#define FAILURE_EXIT(code, format, ...) { fprintf(stderr, format, ##__VA_ARGS__); exit(code);}
+#define BUFF_SIZE 256
 
 int main(int argc, char *argv[]) {
+    srand((unsigned int) (time(NULL) * getpid()));
+    FILE *date;
+
+    if (argc < 3) {
+        kill(getppid(), SIGRTMIN);
+        FAILURE_EXIT(1, "To little Arguments.\n");
+    }
+    int N = (int) strtol(argv[2], '\0', 10);
+
+    int fifo = open(argv[1], O_WRONLY);
+    if (fifo < 0) {
+        kill(getppid(), SIGRTMIN);
+        FAILURE_EXIT(1, "Error opening FIFO\n")
+    }
+
+
+    kill(getppid(), SIGRTMIN);
+
+    printf("Slave %d: %d\n", N, getpid());
+
+    char buffer[2][BUFF_SIZE];
+    for (int i = 0; i < N; i++) {
+        date = popen("date", "r");
+        fgets(buffer[0], BUFF_SIZE, date);
+        sprintf(buffer[1], "Slave: %d - %s", getpid(), buffer[0]);
+        write(fifo, buffer[1], strlen(buffer[1]));
+        fclose(date);
+        sleep((unsigned int) (rand() % 4 + 2));
+    }
+
+    close(fifo);
 
     return 0;
 }
