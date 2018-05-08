f5ae7f0357f6454ffdd2ad773cc382be66491ed0
Malenczuk <malen11pl@gmail.com>
2018-05-08 22:57:44 +0200

    kolosyf

--- kolosyf/zadania/zad2/main.c
+++ kolosyf/zadania/zad2/main.c
@@ -24,30 +24,23 @@ int main(int argc, char* argv[])
  pid_t pid;
 
     if((pid = fork()) == 0) {
-        int val2;
         close(toChildFD[1]);
         close(toParentFD[0]);
-        char buf[128];
-        read(toChildFD[0], buf, 128);
-        val2 = atoi(buf);
+        read(toChildFD[0], &val2, sizeof(int));
     //odczytaj z potoku nienazwanego wartosc przekazana przez proces macierzysty i zapisz w zmiennej val2
         val2 = val2 * val2;
-        sprintf(buf, "%d", val2);
-        write(toParentFD[1], buf, strlen(buf));
+        write(toParentFD[1], &val2, sizeof(int));
     //wyslij potokiem nienazwanym val2 do procesu macierzysego
 
  } else {
         close(toParentFD[1]);
         close(toChildFD[0]);
         val1 = atoi(argv[1]);
-        char buf[128];
-        sprintf(buf, "%d", val1);
-        write(toChildFD[1], buf, strlen(buf));
+        write(toChildFD[1], &val1, sizeof(int));
     //wyslij val1 potokiem nienazwanym do priocesu potomnego
  
      sleep(1);
-        read(toParentFD[0], buf, 128);
-        int val3 = atoi(buf);
+        read(toParentFD[0], &val3, sizeof(int));
     //odczytaj z potoku nienazwanego wartosc przekazana przez proces potomny i zapisz w zmiennej val3
     
      printf("%d square is: %d\n",val1, val3);
